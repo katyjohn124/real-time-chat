50f83a3d784b50bf7cd0b62621e1229ad38fef80
katyjohn124 <148299728+katyjohn124@users.noreply.github.com>
2023-12-09 17:20:23 +0800

    initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>real time chat happy</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+
+};
+</script>
+
+<style lang="scss" scoped></style>
